dd748316461faa08c733233d30901f129ed8fdf5
LyrionTannister <45819677+LyrionTannister@users.noreply.github.com>
2022-12-23 22:20:20 +0300

    Update version.py

--- openstack/version.py
+++ openstack/version.py
@@ -13,4 +13,4 @@
 
 import pbr.version
 
-__version__ = pbr.version.VersionInfo('huaweicloud-sdk-python').version_string()
+__version__ = pbr.version.VersionInfo('cloud-sdk-python').version_string()
